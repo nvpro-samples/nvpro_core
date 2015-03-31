8a99f9213855f101c91c36379e447f449fc406f1
NVIDIA Professional Graphics Samples <nvpro-samples@nvidia.com>
2015-03-31 13:35:24 -0400

    CMake build CUDA find fix
    - Finds CUDA binaries regardless of CUDA version (previously it only worked for 5.5)

--- cmake/FindCuda.cmake
+++ cmake/FindCuda.cmake
@@ -3,6 +3,7 @@
 unset(CUDA_DLL CACHE)
 unset(CUDA_ROOT_DIR CACHE)
 unset(CUDA_LIB CACHE)
+unset(CUDA_VERSION CACHE)
 unset(CUDA_FOUND CACHE)
 unset(RESULT CACHE)
 
@@ -41,12 +42,37 @@ macro(_find_files targetVar incDir dllName dllName64 folder result)
 endmacro()
 
 if(CUDA_ROOT_DIR)
+    #---------Retrieve VERSION
+	set( CUDA_VERSION_MAX "0.0" )
+    string( REGEX REPLACE "(^.*)/[^/]*$" "\\1" CUDA_GENERAL_DIR ${CUDA_ROOT_DIR} )
+	file( GLOB CUDA_VERSION_PATHS ${CUDA_GENERAL_DIR}/* )
+	foreach( CUDA_VERSION_PATH ${CUDA_VERSION_PATHS} )
+	  if( IS_DIRECTORY ${CUDA_VERSION_PATH} )
+        string( REGEX REPLACE "^.*/v([^/]*)$" "\\1" CUDA_VERSION_CURRENT ${CUDA_VERSION_PATH} )	
+	      if( CUDA_VERSION_CURRENT MATCHES "[0-9]+[.]+[0-9]+" )	
+		    if( ${CUDA_VERSION_MAX} VERSION_LESS ${CUDA_VERSION_CURRENT} )
+		      set( CUDA_VERSION_MAX ${CUDA_VERSION_CURRENT} ) 
+		    endif()
+		  endif()
+	  endif()
+	endforeach()
+
+    SET(CUDA_VERSION ${CUDA_VERSION_MAX} CACHE STRING "string")
+    string( REGEX REPLACE "(^[^.]*)[.]+.*$" "\\1" CUDA_VERSION_MAJOR ${CUDA_VERSION} )
+    string( REGEX REPLACE "^[^.]*[.]+(.*$)" "\\1" CUDA_VERSION_MINOR ${CUDA_VERSION} )
+	
+	if( CUDA_VERSION_MAX STREQUAL "0.0" )
+	  message( "CUDA directory is not valid" )
+	else()
+	  set( CUDA_ROOT_DIR ${CUDA_GENERAL_DIR}/v${CUDA_VERSION} )
+	endif()
+
 
 	#-------- Locate DLLS
-    _find_files( CUDA_DLL CUDA_ROOT_DIR "cudart32_55.dll" "cudart64_55.dll" "bin/" RESULT)	
+    _find_files( CUDA_DLL CUDA_ROOT_DIR "cudart32_${CUDA_VERSION_MAJOR}${CUDA_VERSION_MINOR}.dll" "cudart64_${CUDA_VERSION_MAJOR}${CUDA_VERSION_MINOR}.dll" "bin/" RESULT)	
 	if ( ${RESULT} STREQUAL "NOTFOUND" )
-	   message( "CUDA 5.5 is required for this sample (${PROJECT_NAME})." )
-	   message( "Please set the CUDA_LOCATION variable above to the root location of CUDA 5.5." )
+	   message( "CUDA is required for this sample (${PROJECT_NAME})." )
+	   message( "CUDA binaries have not been found. Please set the CUDA_LOCATION variable above to the root location of the desired CUDA version." )
 	endif()
 	
 	#-------- Locate LIBS
