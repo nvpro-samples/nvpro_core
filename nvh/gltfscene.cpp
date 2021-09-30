ec454643a46d7d036724001ac3e453ea47787b70
mklefrancois <mlefrancois@nvidia.com>
2021-09-30 14:09:20 +0200

    Fixed scene bbox computation

--- nvh/gltfscene.cpp
+++ nvh/gltfscene.cpp
@@ -22,12 +22,94 @@
 #include "nvprint.hpp"
 #include <iostream>
 #include <numeric>
+#include <limits>
 #include <set>
 #include <sstream>
 
 namespace nvh {
 
 #define EXTENSION_ATTRIB_IRAY "NV_attributes_iray"
+
+
+struct Bbox
+{
+  Bbox() = default;
+  Bbox(nvmath::vec3f _min, nvmath::vec3f _max)
+      : m_min(_min)
+      , m_max(_max)
+  {
+  }
+  Bbox(const std::vector<nvmath::vec3f>& corners)
+  {
+    for(auto& c : corners)
+    {
+      insert(c);
+    }
+  }
+
+  void insert(const nvmath::vec3f& v)
+  {
+    m_min = {std::min(m_min.x, v.x), std::min(m_min.y, v.y), std::min(m_min.z, v.z)};
+    m_max = {std::max(m_max.x, v.x), std::max(m_max.y, v.y), std::max(m_max.z, v.z)};
+  }
+
+  void insert(const Bbox& b)
+  {
+    insert(b.m_min);
+    insert(b.m_max);
+  }
+
+  inline Bbox& operator+=(float v)
+  {
+    m_min -= v;
+    m_max += v;
+    return *this;
+  }
+
+  inline bool isEmpty() const
+  {
+    return m_min == nvmath::vec3f{std::numeric_limits<float>::max()}
+           || m_max == nvmath::vec3f{std::numeric_limits<float>::lowest()};
+  }
+  inline uint32_t rank() const
+  {
+    uint32_t result{0};
+    result += m_min.x < m_max.x;
+    result += m_min.y < m_max.y;
+    result += m_min.z < m_max.z;
+    return result;
+  }
+  inline bool          isPoint() const { return m_min == m_max; }
+  inline bool          isLine() const { return rank() == 1u; }
+  inline bool          isPlane() const { return rank() == 2u; }
+  inline bool          isVolume() const { return rank() == 3u; }
+  inline nvmath::vec3f min() { return m_min; }
+  inline nvmath::vec3f max() { return m_max; }
+  inline nvmath::vec3f extents() { return m_max - m_min; }
+  inline nvmath::vec3f center() { return (m_min + m_max) * 0.5f; }
+  inline float         radius() { return nvmath::length(m_max - m_min) * 0.5f; }
+
+  Bbox transform(nvmath::mat4f mat)
+  {
+    std::vector<nvmath::vec3f> corners(8);
+    corners[0] = mat * nvmath::vec3f(m_min.x, m_min.y, m_min.z);
+    corners[1] = mat * nvmath::vec3f(m_min.x, m_min.y, m_max.z);
+    corners[2] = mat * nvmath::vec3f(m_min.x, m_max.y, m_min.z);
+    corners[3] = mat * nvmath::vec3f(m_min.x, m_max.y, m_max.z);
+    corners[4] = mat * nvmath::vec3f(m_max.x, m_min.y, m_min.z);
+    corners[5] = mat * nvmath::vec3f(m_max.x, m_min.y, m_max.z);
+    corners[6] = mat * nvmath::vec3f(m_max.x, m_max.y, m_min.z);
+    corners[7] = mat * nvmath::vec3f(m_max.x, m_max.y, m_max.z);
+
+    Bbox result(corners);
+    return result;
+  }
+
+private:
+  nvmath::vec3f m_min{std::numeric_limits<float>::max()};
+  nvmath::vec3f m_max{std::numeric_limits<float>::lowest()};
+};
+
 
 //--------------------------------------------------------------------------------------------------
 // Collect the value of all materials
@@ -85,8 +167,8 @@ void GltfScene::importMaterials(const tinygltf::Model& tmodel)
       // Computing the transformation
       auto translation = nvmath::mat3f(1, 0, tt.offset.x, 0, 1, tt.offset.y, 0, 0, 1);
       auto rotation = nvmath::mat3f(cos(tt.rotation), sin(tt.rotation), 0, -sin(tt.rotation), cos(tt.rotation), 0, 0, 0, 1);
-      auto scale       = nvmath::mat3f(tt.scale.x, 0, 0, 0, tt.scale.y, 0, 0, 0, 1);
-      tt.uvTransform   = scale * rotation * translation;
+      auto scale     = nvmath::mat3f(tt.scale.x, 0, 0, 0, tt.scale.y, 0, 0, 0, 1);
+      tt.uvTransform = scale * rotation * translation;
     }
 
     // KHR_materials_unlit
@@ -665,28 +747,29 @@ void GltfScene::destroy()
 //
 void GltfScene::computeSceneDimensions()
 {
-  auto valMin = nvmath::vec3f(FLT_MAX);
-  auto valMax = nvmath::vec3f(-FLT_MAX);
+  Bbox scnBbox;
+
   for(const auto& node : m_nodes)
   {
     const auto& mesh = m_primMeshes[node.primMesh];
 
-    nvmath::vec4f locMin = node.worldMatrix * nvmath::vec4f(mesh.posMin, 1.0f);
-    nvmath::vec4f locMax = node.worldMatrix * nvmath::vec4f(mesh.posMax, 1.0f);
+    Bbox bbox(mesh.posMin, mesh.posMax);
+    bbox.transform(node.worldMatrix);
+    scnBbox.insert(bbox);
+  }
 
-    valMin = {std::min(locMin.x, valMin.x), std::min(locMin.y, valMin.y), std::min(locMin.z, valMin.z)};
-    valMax = {std::max(locMax.x, valMax.x), std::max(locMax.y, valMax.y), std::max(locMax.z, valMax.z)};
-  }
-  if(valMin == valMax)
+  if(scnBbox.isEmpty() || !scnBbox.isVolume())
   {
-    valMin = nvmath::vec3f(-1);
-    valMin = nvmath::vec3f(1);
+    LOGE("glTF: Scene bounding box invalid, Setting to: [-1,-1,-1], [1,1,1]");
+    scnBbox.insert({-1.0f, -1.0f, -1.0f});
+    scnBbox.insert({1.0f, 1.0f, 1.0f});
   }
-  m_dimensions.min    = valMin;
-  m_dimensions.max    = valMax;
-  m_dimensions.size   = valMax - valMin;
-  m_dimensions.center = (valMin + valMax) / 2.0f;
-  m_dimensions.radius = nvmath::length(valMax - valMin) / 2.0f;
+
+  m_dimensions.min    = scnBbox.min();
+  m_dimensions.max    = scnBbox.max();
+  m_dimensions.size   = scnBbox.extents();
+  m_dimensions.center = scnBbox.center();
+  m_dimensions.radius = scnBbox.radius();
 }
 
 
